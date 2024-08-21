12a54f404314f6a70da5bb969954c94578f636df
Udit <25996904+uditdc@users.noreply.github.com>
2024-08-21 12:53:30 +0530

    Include staker node coordinator

--- src/core/StakerNodeCoordinator.sol
+++ src/core/StakerNodeCoordinator.sol
@@ -1,36 +1,75 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin-upgradeable/contracts/access/AccessControlUpgradeable.sol";
+import {IStrategyManager} from "@eigenlayer/contracts/interfaces/IStrategyManager.sol";
+import {IDelegationManager} from "@eigenlayer/contracts/interfaces/IDelegationManager.sol";
 
-import {LiquidToken} from "./LiquidToken.sol";
 import {StakerNode} from "./StakerNode.sol";
+import {IOrchestrator} from "../interfaces/IOrchestrator.sol";
 import {IStakerNodeCoordinator} from "../interfaces/IStakerNodeCoordinator.sol";
 
-contract StakerNodeCoordinator is IStakerNodeCoordinator, Ownable {
-    LiquidToken public lToken;
+contract StakerNodeCoordinator is
+    IStakerNodeCoordinator,
+    AccessControlUpgradeable
+{
+    IOrchestrator public orchestrator;
+    IStrategyManager public strategyManager;
+    IDelegationManager public delegationManager;
+    uint256 public maxNodes;
+
     address[] public stakerNodes;
 
-    constructor(address _lToken) Ownable(msg.sender) {
-        lToken = LiquidToken(_lToken);
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    constructor() {
+        _disableInitializers();
     }
 
-    function addStakerNode(address _nodeAddress) external onlyOwner {
-        require(_nodeAddress != address(0), "Invalid node address");
+    function initialize(Init calldata init) external initializer {
+        __AccessControl_init();
+
+        _grantRole(ADMIN_ROLE, init.initialOwner);
+        _grantRole(PAUSER_ROLE, init.pauser);
+
+        orchestrator = init.orchestrator;
+        delegationManager = init.delegationManager;
+        strategyManager = init.strategyManager;
+        maxNodes = init.maxNodes;
+    }
+
+    function addStakerNode(address _nodeAddress) external onlyRole(ADMIN_ROLE) {
+        if (_nodeAddress == address(0)) {
+            revert ZeroAddress();
+        }
+
+        if (stakerNodes.length >= maxNodes) {
+            revert MaxNodesReached(maxNodes);
+        }
+
+        for (uint i = 0; i < stakerNodes.length; i++) {
+            if (stakerNodes[i] == _nodeAddress) {
+                revert NodeAlreadyRegistered(_nodeAddress);
+            }
+        }
+
         stakerNodes.push(_nodeAddress);
         emit StakerNodeAdded(_nodeAddress);
     }
 
-    // function withdrawToNode(
-    //     address _nodeAddress,
-    //     uint256 _amount
-    // ) external onlyOwner {
-    //     require(_nodeAddress != address(0), "Invalid node address");
-    //     require(isStakerNode(_nodeAddress), "Not a registered staker node");
-
-    //     lToken.withdrawToNode(_nodeAddress, _amount);
-    //     emit TokensWithdrawnToNode(_nodeAddress, _amount);
-    // }
+    function removeStakerNode(
+        address _nodeAddress
+    ) external onlyRole(ADMIN_ROLE) {
+        for (uint i = 0; i < stakerNodes.length; i++) {
+            if (stakerNodes[i] == _nodeAddress) {
+                stakerNodes[i] = stakerNodes[stakerNodes.length - 1];
+                stakerNodes.pop();
+                emit StakerNodeRemoved(_nodeAddress);
+                return;
+            }
+        }
+    }
 
     function isStakerNode(address _nodeAddress) public view returns (bool) {
         for (uint i = 0; i < stakerNodes.length; i++) {
@@ -43,5 +82,10 @@ contract StakerNodeCoordinator is IStakerNodeCoordinator, Ownable {
 
     function getStakerNodesCount() external view returns (uint256) {
         return stakerNodes.length;
+    }
+
+    function setMaxNodes(uint256 _maxNodes) external onlyRole(ADMIN_ROLE) {
+        maxNodes = _maxNodes;
+        emit MaxNodesUpdated(_maxNodes);
     }
 }
--- src/interfaces/IStakerNodeCoordinator.sol
+++ src/interfaces/IStakerNodeCoordinator.sol
@@ -1,7 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {IStrategyManager} from "@eigenlayer/contracts/interfaces/IStrategyManager.sol";
+import {IDelegationManager} from "@eigenlayer/contracts/interfaces/IDelegationManager.sol";
+import {IOrchestrator} from "../interfaces/IOrchestrator.sol";
+
 interface IStakerNodeCoordinator {
-    event StakerNodeAdded(address indexed nodeAddress);
+    struct Init {
+        IOrchestrator orchestrator;
+        IDelegationManager delegationManager;
+        IStrategyManager strategyManager;
+        uint256 maxNodes;
+        address initialOwner;
+        address pauser;
+        address unpauser;
+    }
+
+    event StakerNodeAdded(address nodeAddress);
+    event StakerNodeRemoved(address nodeAddress);
+    event MaxNodesUpdated(uint256 maxNodes);
     event TokensWithdrawnToNode(address indexed nodeAddress, uint256 amount);
+
+    error ZeroAddress();
+    error MaxNodesReached(uint256 maxNodes);
+    error NodeAlreadyRegistered(address nodeAddress);
+    error NotOwner();
 }
