511f043d152ecb32b4d71576c9e8a0722927c6c4
Udit <25996904+uditdc@users.noreply.github.com>
2024-08-27 11:16:11 +0530

    Updates to the staker node coordiantor logic

--- src/core/StakerNodeCoordinator.sol
+++ src/core/StakerNodeCoordinator.sol
@@ -2,90 +2,219 @@
 pragma solidity ^0.8.20;
 
 import {AccessControlUpgradeable} from "@openzeppelin-upgradeable/contracts/access/AccessControlUpgradeable.sol";
+import {BeaconProxy} from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import {IStakerNodeCoordinator} from "../interfaces/IStakerNodeCoordinator.sol";
+import {IStakerNode} from "../interfaces/IStakerNode.sol";
+import {IOrchestrator} from "../interfaces/IOrchestrator.sol";
 import {IStrategyManager} from "@eigenlayer/contracts/interfaces/IStrategyManager.sol";
 import {IDelegationManager} from "@eigenlayer/contracts/interfaces/IDelegationManager.sol";
-
-import {StakerNode} from "./StakerNode.sol";
-import {IOrchestrator} from "../interfaces/IOrchestrator.sol";
-import {IStakerNodeCoordinator} from "../interfaces/IStakerNodeCoordinator.sol";
 
 contract StakerNodeCoordinator is
     IStakerNodeCoordinator,
     AccessControlUpgradeable
 {
-    IOrchestrator public orchestrator;
-    IStrategyManager public strategyManager;
-    IDelegationManager public delegationManager;
-    uint256 public maxNodes;
+    IOrchestrator public override orchestrator;
+    IStrategyManager public override strategyManager;
+    IDelegationManager public override delegationManager;
+    uint256 public override maxNodes;
 
-    address[] public stakerNodes;
+    UpgradeableBeacon public upgradeableBeacon;
+    address[] private stakerNodes;
 
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant STAKER_NODE_OPERATOR_ROLE =
+        keccak256("STAKER_NODE_OPERATOR_ROLE");
+    bytes32 public constant STAKER_NODES_DELEGATOR_ROLE =
+        keccak256("STAKER_NODES_DELEGATOR_ROLE");
+    bytes32 public constant STAKER_NODE_CREATOR_ROLE =
+        keccak256("STAKER_NODE_CREATOR_ROLE");
 
     constructor() {
         _disableInitializers();
     }
 
-    function initialize(Init calldata init) external initializer {
+    /// @notice Initializes the StakerNodeCoordinator contract
+    /// @param init Struct containing initialization parameters
+    /// @dev This function can only be called once due to the initializer modifier
+    function initialize(Init calldata init) external override initializer {
         __AccessControl_init();
 
         _grantRole(ADMIN_ROLE, init.initialOwner);
-        _grantRole(PAUSER_ROLE, init.pauser);
+        _grantRole(STAKER_NODE_OPERATOR_ROLE, init.stakerNodeOperator);
+        _grantRole(STAKER_NODE_CREATOR_ROLE, init.stakerNodeCreator);
+        _grantRole(STAKER_NODES_DELEGATOR_ROLE, init.stakerNodesDelegator);
 
         orchestrator = init.orchestrator;
-        delegationManager = init.delegationManager;
         strategyManager = init.strategyManager;
+        delegationManager = init.delegationManager;
         maxNodes = init.maxNodes;
     }
 
-    function addStakerNode(address _nodeAddress) external onlyRole(ADMIN_ROLE) {
-        if (_nodeAddress == address(0)) {
-            revert ZeroAddress();
+    /// @notice Creates a new staker node
+    /// @dev Only callable by accounts with STAKER_NODE_CREATOR_ROLE
+    /// @return Address of the newly created staker node
+    function createStakerNode()
+        public
+        override
+        notZeroAddress(address(upgradeableBeacon))
+        onlyRole(STAKER_NODE_CREATOR_ROLE)
+        returns (address)
+    {
+        uint256 nodeId = stakerNodes.length;
+
+        if (nodeId >= maxNodes) {
+            revert TooManyStakerNodes(maxNodes);
         }
 
-        if (stakerNodes.length >= maxNodes) {
-            revert MaxNodesReached(maxNodes);
-        }
+        BeaconProxy proxy = new BeaconProxy(address(upgradeableBeacon), "");
+        address node = address(proxy);
 
-        for (uint i = 0; i < stakerNodes.length; i++) {
-            if (stakerNodes[i] == _nodeAddress) {
-                revert NodeAlreadyRegistered(_nodeAddress);
-            }
-        }
+        initializeStakerNode(IStakerNode(node), nodeId);
 
-        stakerNodes.push(_nodeAddress);
-        emit StakerNodeAdded(_nodeAddress);
+        stakerNodes.push(node);
+
+        emit StakerNodeCreated(nodeId, node);
+
+        return node;
     }
 
-    function removeStakerNode(
-        address _nodeAddress
-    ) external onlyRole(ADMIN_ROLE) {
-        for (uint i = 0; i < stakerNodes.length; i++) {
-            if (stakerNodes[i] == _nodeAddress) {
-                stakerNodes[i] = stakerNodes[stakerNodes.length - 1];
-                stakerNodes.pop();
-                emit StakerNodeRemoved(_nodeAddress);
-                return;
-            }
+    /// @notice Initializes a staker node
+    /// @param node The staker node to initialize
+    /// @param nodeId The ID of the staker node
+    /// @dev This function is internal and called during node creation and upgrades
+    function initializeStakerNode(IStakerNode node, uint256 nodeId) internal {
+        uint64 initializedVersion = node.getInitializedVersion();
+        if (initializedVersion == 0) {
+            node.initialize(
+                IStakerNode.Init(IStakerNodeCoordinator(address(this)), nodeId)
+            );
+
+            initializedVersion = node.getInitializedVersion();
+            emit NodeInitialized(address(node), initializedVersion);
         }
     }
 
-    function isStakerNode(address _nodeAddress) public view returns (bool) {
-        for (uint i = 0; i < stakerNodes.length; i++) {
-            if (stakerNodes[i] == _nodeAddress) {
-                return true;
-            }
+    /// @notice Registers the initial staker node implementation
+    /// @param _implementationContract Address of the implementation contract
+    /// @dev Can only be called once by an account with ADMIN_ROLE
+    function registerStakerNodeImplementation(
+        address _implementationContract
+    )
+        public
+        override
+        onlyRole(ADMIN_ROLE)
+        notZeroAddress(_implementationContract)
+    {
+        if (address(upgradeableBeacon) != address(0)) {
+            revert BeaconImplementationAlreadyExists();
         }
-        return false;
+
+        upgradeableBeacon = new UpgradeableBeacon(
+            _implementationContract,
+            address(this)
+        );
+
+        emit RegisteredStakerNodeImplementation(
+            address(upgradeableBeacon),
+            _implementationContract
+        );
     }
 
-    function getStakerNodesCount() external view returns (uint256) {
+    /// @notice Upgrades the staker node implementation
+    /// @param _implementationContract Address of the new implementation contract
+    /// @dev Can only be called by an account with ADMIN_ROLE
+    function upgradeStakerNodeImplementation(
+        address _implementationContract
+    )
+        public
+        override
+        onlyRole(ADMIN_ROLE)
+        notZeroAddress(_implementationContract)
+    {
+        if (address(upgradeableBeacon) == address(0)) {
+            revert NoBeaconImplementationExists();
+        }
+
+        upgradeableBeacon.upgradeTo(_implementationContract);
+
+        uint256 nodeCount = stakerNodes.length;
+
+        for (uint256 i = 0; i < nodeCount; i++) {
+            initializeStakerNode(IStakerNode(stakerNodes[i]), i);
+        }
+
+        emit UpgradedStakerNodeImplementation(
+            _implementationContract,
+            nodeCount
+        );
+    }
+
+    /// @notice Sets the maximum number of staker nodes
+    /// @param _maxNodes New maximum number of nodes
+    /// @dev Can only be called by an account with ADMIN_ROLE
+    function setMaxNodes(
+        uint256 _maxNodes
+    ) public override onlyRole(ADMIN_ROLE) {
+        maxNodes = _maxNodes;
+        emit MaxNodesUpdated(_maxNodes);
+    }
+
+    /// @notice Checks if an account has the STAKER_NODE_OPERATOR_ROLE
+    /// @param account Address to check
+    /// @return bool True if the account has the role, false otherwise
+    function hasStakerNodeOperatorRole(
+        address account
+    ) external view override returns (bool) {
+        return hasRole(STAKER_NODE_OPERATOR_ROLE, account);
+    }
+
+    /// @notice Checks if an address has the STAKER_NODES_DELEGATOR_ROLE
+    /// @param _address Address to check
+    /// @return bool True if the address has the role, false otherwise
+    function hasStakerNodeDelegatorRole(
+        address _address
+    ) public view override returns (bool) {
+        return hasRole(STAKER_NODES_DELEGATOR_ROLE, _address);
+    }
+
+    /// @notice Checks if a caller is the orchestrator
+    /// @param caller Address to check
+    /// @return bool True if the caller is the orchestrator, false otherwise
+    function hasOrchestratorRole(
+        address caller
+    ) public view override returns (bool) {
+        return caller == address(orchestrator);
+    }
+
+    /// @notice Retrieves all staker node addresses
+    /// @return address[] Array of all staker node addresses
+    function getAllNodes() public view override returns (address[] memory) {
+        return stakerNodes;
+    }
+
+    /// @notice Gets the total number of staker nodes
+    /// @return uint256 The number of staker nodes
+    function getStakerNodesCount() public view override returns (uint256) {
         return stakerNodes.length;
     }
 
-    function setMaxNodes(uint256 _maxNodes) external onlyRole(ADMIN_ROLE) {
-        maxNodes = _maxNodes;
-        emit MaxNodesUpdated(_maxNodes);
+    /// @notice Retrieves a staker node address by its ID
+    /// @param nodeId The ID of the staker node
+    /// @return address The address of the staker node
+    function getNodeById(
+        uint256 nodeId
+    ) public view override returns (address) {
+        if (nodeId >= stakerNodes.length) {
+            revert NodeIdOutOfRange(nodeId);
+        }
+        return stakerNodes[nodeId];
+    }
+
+    modifier notZeroAddress(address _address) {
+        if (_address == address(0)) {
+            revert ZeroAddress();
+        }
+        _;
     }
 }
--- src/interfaces/IStakerNodeCoordinator.sol
+++ src/interfaces/IStakerNodeCoordinator.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.8.20;
 import {IStrategyManager} from "@eigenlayer/contracts/interfaces/IStrategyManager.sol";
 import {IDelegationManager} from "@eigenlayer/contracts/interfaces/IDelegationManager.sol";
 import {IOrchestrator} from "../interfaces/IOrchestrator.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/// @title IStakerNodeCoordinator
+/// @notice Interface for the StakerNodeCoordinator contract
 interface IStakerNodeCoordinator {
+    /// @notice Initialization parameters for the StakerNodeCoordinator
     struct Init {
         IOrchestrator orchestrator;
         IDelegationManager delegationManager;
@@ -14,18 +18,113 @@ interface IStakerNodeCoordinator {
         address initialOwner;
         address pauser;
         address unpauser;
+        address stakerNodeOperator;
+        address stakerNodeCreator;
+        address stakerNodesDelegator;
     }
 
-    event StakerNodeAdded(address nodeAddress);
-    event StakerNodeRemoved(address nodeAddress);
-    event MaxNodesUpdated(uint256 maxNodes);
-    event TokensWithdrawnToNode(address indexed nodeAddress, uint256 amount);
+    /// @notice Emitted when a new staker node is created
+    /// @param nodeId The ID of the newly created node
+    /// @param nodeAddress The address of the newly created node
+    event StakerNodeCreated(uint256 nodeId, address nodeAddress);
 
+    /// @notice Emitted when a staker node is removed
+    /// @param nodeAddress The address of the removed node
+    event StakerNodeRemoved(address nodeAddress);
+
+    /// @notice Emitted when the maximum number of nodes is updated
+    /// @param maxNodes The new maximum number of nodes
+    event MaxNodesUpdated(uint256 maxNodes);
+
+    /// @notice Emitted when a new staker node implementation is registered
+    /// @param upgradeableBeaconAddress The address of the upgradeable beacon
+    /// @param implementationContract The address of the new implementation contract
+    event RegisteredStakerNodeImplementation(address upgradeableBeaconAddress, address implementationContract);
+
+    /// @notice Emitted when the staker node implementation is upgraded
+    /// @param implementationContract The address of the new implementation contract
+    /// @param nodesCount The number of nodes upgraded
+    event UpgradedStakerNodeImplementation(address implementationContract, uint256 nodesCount);
+
+    /// @notice Emitted when a node is initialized
+    /// @param nodeAddress The address of the initialized node
+    /// @param initializedVersion The version of the initialization
+    event NodeInitialized(address nodeAddress, uint64 initializedVersion);
+
+    error UnsupportedAsset(IERC20 asset);
+    error Unauthorized();
+    error InsufficientFunds();
+    error Paused();
+    error ZeroAmount();
     error ZeroAddress();
-    error MaxNodesReached(uint256 maxNodes);
+    error BeaconImplementationAlreadyExists();
+    error NoBeaconImplementationExists();
+    error TooManyStakerNodes(uint256 maxNodes);
+    error NodeIdOutOfRange(uint256 nodeId);
     error NodeAlreadyRegistered(address nodeAddress);
     error NotOwner();
 
+    /// @notice Initializes the StakerNodeCoordinator contract
+    /// @param init Struct containing initialization parameters
+    function initialize(Init calldata init) external;
+
+    /// @notice Creates a new staker node
+    /// @return The address of the newly created staker node
+    function createStakerNode() external returns (address);
+
+    /// @notice Registers the initial staker node implementation
+    /// @param _implementationContract Address of the implementation contract
+    function registerStakerNodeImplementation(address _implementationContract) external;
+
+    /// @notice Upgrades the staker node implementation
+    /// @param _implementationContract Address of the new implementation contract
+    function upgradeStakerNodeImplementation(address _implementationContract) external;
+
+    /// @notice Sets the maximum number of staker nodes
+    /// @param _maxNodes New maximum number of nodes
+    function setMaxNodes(uint256 _maxNodes) external;
+
+    /// @notice Checks if an account has the STAKER_NODE_OPERATOR_ROLE
+    /// @param account Address to check
+    /// @return True if the account has the role, false otherwise
+    function hasStakerNodeOperatorRole(address account) external view returns (bool);
+
+    /// @notice Checks if an address has the STAKER_NODES_DELEGATOR_ROLE
+    /// @param _address Address to check
+    /// @return True if the address has the role, false otherwise
+    function hasStakerNodeDelegatorRole(address _address) external view returns (bool);
+
+    /// @notice Checks if a caller is the orchestrator
+    /// @param caller Address to check
+    /// @return True if the caller is the orchestrator, false otherwise
+    function hasOrchestratorRole(address caller) external view returns (bool);
+
+    /// @notice Retrieves all staker node addresses
+    /// @return An array of all staker node addresses
+    function getAllNodes() external view returns (address[] memory);
+
+    /// @notice Gets the total number of staker nodes
+    /// @return The number of staker nodes
+    function getStakerNodesCount() external view returns (uint256);
+
+    /// @notice Retrieves a staker node address by its ID
+    /// @param nodeId The ID of the staker node
+    /// @return The address of the staker node
+    function getNodeById(uint256 nodeId) external view returns (address);
+
+    /// @notice Gets the delegation manager contract
+    /// @return The IDelegationManager interface
     function delegationManager() external view returns (IDelegationManager);
+
+    /// @notice Gets the strategy manager contract
+    /// @return The IStrategyManager interface
     function strategyManager() external view returns (IStrategyManager);
+
+    /// @notice Gets the orchestrator contract
+    /// @return The IOrchestrator interface
+    function orchestrator() external view returns (IOrchestrator);
+
+    /// @notice Gets the maximum number of nodes allowed
+    /// @return The maximum number of nodes
+    function maxNodes() external view returns (uint256);
 }
