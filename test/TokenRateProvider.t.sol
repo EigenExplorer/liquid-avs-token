2c0656cb411fad12572c3529a6917d7c3b035645
surbhit14 <surbhitagrawal716@gmail.com>
2024-09-05 07:54:58 +0530

    Test Coverage for Token Functions

--- test/TokenRateProvider.t.sol
+++ test/TokenRateProvider.t.sol
@@ -6,6 +6,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {BaseTest} from "./common/BaseTest.sol";
 import {TokenRegistryOracle} from "../src/utils/TokenRegistryOracle.sol";
 import {ITokenRegistryOracle} from "../src/interfaces/ITokenRegistryOracle.sol";
+import {TokenRegistry} from "../src/utils/TokenRegistry.sol";
+import {ITokenRegistry} from "../src/interfaces/ITokenRegistry.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {MockERC20} from "./mocks/MockERC20.sol";
 
 contract TokenRateProviderTest is BaseTest {
     function setUp() public override {
@@ -98,4 +102,129 @@ contract TokenRateProviderTest is BaseTest {
         vm.expectRevert(); // TODO: Check revert message
         tokenRegistryOracle.batchUpdateRates(tokens, rates);
     }
+
+    function testRemoveTokenSuccess() public {
+        tokenRegistry.removeToken(testToken);
+        assertFalse(tokenRegistry.tokenIsSupported(testToken), "Token should be removed");
+
+        IERC20[] memory supportedTokens = tokenRegistry.getSupportedTokens();
+        for (uint256 i = 0; i < supportedTokens.length; i++) {
+            assertFalse(supportedTokens[i] == testToken, "Token should be removed from the supported tokens array");
+        }
+    }
+
+    function testRemoveTokenFailsForUnsupportedToken() public {
+        tokenRegistry.removeToken(testToken);
+        assertFalse(tokenRegistry.tokenIsSupported(testToken), "Token should not be supported before attempting to remove");
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ITokenRegistry.TokenNotSupported.selector,
+                address(testToken)
+            )
+        );
+        tokenRegistry.removeToken(testToken);
+    }
+
+    function testRemoveTokenFailsForNonAdmin() public {
+        assertTrue(tokenRegistry.tokenIsSupported(testToken), "Token should be supported before non-admin attempts to remove");
+        vm.prank(user1); 
+        vm.expectRevert();
+        tokenRegistry.removeToken(testToken);
+    }
+
+    function testAddTokenSuccess() public {
+        IERC20 newToken = IERC20(address(new MockERC20("New Token", "NEW")));
+        uint256 decimals = 18;
+        uint256 initialPrice = 1e18;
+
+        tokenRegistry.addToken(newToken, decimals, initialPrice);
+
+        // Verify that the token was successfully added
+        ITokenRegistry.TokenInfo memory tokenInfo = tokenRegistry.getTokenInfo(newToken);
+        assertEq(tokenInfo.decimals, decimals, "Incorrect decimals");
+        assertEq(tokenInfo.pricePerUnit, initialPrice, "Incorrect initial price");
+
+        // Verify that the token is now supported
+        assertTrue(tokenRegistry.tokenIsSupported(newToken), "Token should be supported");
+
+        // Verify that the token is included in the supportedTokens array
+        IERC20[] memory supportedTokens = tokenRegistry.getSupportedTokens();
+        bool isTokenInArray = false;
+        for (uint256 i = 0; i < supportedTokens.length; i++) {
+            if (supportedTokens[i] == newToken) {
+                isTokenInArray = true;
+                break;
+            }
+        }
+        assertTrue(isTokenInArray, "Token should be in the supportedTokens array");
+    }
+
+    function testAddTokenFailsIfAlreadySupported() public {
+        uint256 decimals = 18;
+        uint256 initialPrice = 1e18;
+
+        // Attempt to add the same token again and expect a revert
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ITokenRegistry.TokenAlreadySupported.selector,
+                address(testToken)
+            )
+        );
+        tokenRegistry.addToken(testToken, decimals, initialPrice);
+    }
+
+    function testAddTokenFailsForZeroDecimals() public {
+        IERC20 newToken = IERC20(address(new MockERC20("New Token", "NEW")));
+        uint256 zeroDecimals = 0;
+        uint256 price = 1e18;
+
+        // Attempt to add the token with zero dedcimals and expect a revert
+        vm.expectRevert(ITokenRegistry.InvalidDecimals.selector);
+        tokenRegistry.addToken(newToken, zeroDecimals, price);
+    }
+
+    function testAddTokenFailsForZeroInitialPrice() public {
+        IERC20 newToken = IERC20(address(new MockERC20("New Token", "NEW")));
+        uint256 decimals = 18;
+        uint256 zeroPrice = 0;
+
+        // Attempt to add the token with zero initial price and expect a revert
+        vm.expectRevert(ITokenRegistry.InvalidPrice.selector);
+        tokenRegistry.addToken(newToken, decimals, zeroPrice);
+    }
+
+    function testUpdatePriceSuccess() public {
+        tokenRegistry.grantRole(tokenRegistry.PRICE_UPDATER_ROLE(), admin);
+        uint256 newPrice = 2e18;
+        tokenRegistry.updatePrice(testToken, newPrice);
+        // Verify the price has been updated correctly
+        assertEq(tokenRegistry.getTokenInfo(testToken).pricePerUnit, newPrice, "Price should be updated successfully");
+    }
+
+    function testUpdatePriceFailsForUnsupportedToken() public {
+        tokenRegistry.grantRole(tokenRegistry.PRICE_UPDATER_ROLE(), admin);
+        // Attempt to update the price of a token that hasn't been added to the registry
+        IERC20 newToken = IERC20(address(new MockERC20("New Token", "NEW")));
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ITokenRegistry.TokenNotSupported.selector,
+                address(newToken)
+            )
+        );
+        tokenRegistry.updatePrice(newToken, 2e18);
+    }
+
+    function testUpdatePriceFailsForZeroPrice() public {
+        tokenRegistry.grantRole(tokenRegistry.PRICE_UPDATER_ROLE(), admin);
+        // Attempt to set the price to zero
+        vm.expectRevert(ITokenRegistry.InvalidPrice.selector);
+        tokenRegistry.updatePrice(testToken, 0);
+    }
+
+    function testUpdatePriceFailsForNonUpdater() public {
+        vm.prank(user1);
+        vm.expectRevert();
+        tokenRegistry.updatePrice(testToken, 2e18);
+    }
+
 }
