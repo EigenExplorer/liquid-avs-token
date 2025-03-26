91c6f2906aeddf23bee8954ff01cedfa5f3f5c23
Gowtham S <gowtham54321@yahoo.com>
2025-03-26 16:13:05 +0530

    refactor: make task k for batch update rates

--- manager/src/workflows/updateAllTokenPrices.ts
+++ manager/src/workflows/updateAllTokenPrices.ts
@@ -1,18 +1,5 @@
-import "dotenv/config";
-
-import { exec } from "node:child_process";
-import { promisify } from "node:util";
-import {
-  DEPLOYMENT,
-  PRICE_UPDATER,
-  LIQUID_TOKEN_ADDRESS,
-  forgeCommand,
-} from "../utils/forge";
-import { getChain, getWalletClient } from "../utils/viemClient";
-import { privateKeyToAccount } from "viem/accounts";
-import fs from "node:fs/promises";
-
-const execAsync = promisify(exec);
+import { DEPLOYMENT, LIQUID_TOKEN_ADDRESS } from "../utils/forge";
+import { batchUpdateRates } from "../tasks/price-updater/batchUpdateRates";
 
 interface Token {
   address: string;
@@ -102,51 +89,7 @@ export async function updateAllTokenPrices() {
       }
     }
 
-    // Setup task params
-    const task = "TRO_BatchUpdateRates.s.sol:BatchUpdateRates";
-    const sender =
-      DEPLOYMENT === "local"
-        ? "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC" // Anvil acc #2
-        : PRICE_UPDATER;
-    const sig = "run(string,address[],uint256[])";
-    const addressesParam = `[${addresses.map((op) => `"${op}"`).join(",")}]`;
-    const pricesParam = `[${prices.join(",")}]`;
-    const params = `${addressesParam} ${pricesParam}`;
-
-    // Simulate task and create transaction
-    const { stdout } = await execAsync(forgeCommand(task, sender, sig, params));
-
-    // Parse the transactions from the forge output
-    const broadcastMatch = stdout.match(/"transactions":"([^"]+)"/);
-
-    if (!broadcastMatch)
-      throw new Error("Could not find broadcast file path in output");
-
-    const broadcastData = JSON.parse(
-      await fs.readFile(broadcastMatch[1].replace(/\\\\/g, "\\"), "utf8")
-    );
-
-    const transactions = broadcastData.transactions;
-
-    if (!transactions || !Array.isArray(transactions))
-      throw new Error("No transactions found");
-
-    const key = process.env.PRICE_UPDATER_PRIVATE_KEY as `0x${string}`;
-    const account = privateKeyToAccount(key);
-    const walletClient = getWalletClient(account);
-
-    // Send tx
-    const txHash = await walletClient.sendTransaction({
-      account,
-      to: transactions[0].to as `0x${string}`,
-      data: transactions[0].input as `0x${string}`,
-      value: BigInt(transactions[0].value || "0x0"),
-      chain: walletClient.chain,
-    });
-
-    console.log(
-      `[Price Updater] Transaction submitted: ${txHash}: tokens updated: ${addresses.length}`
-    );
+    await batchUpdateRates(addresses, prices);
   } catch (error) {
     console.log("Error: ", error);
   }
