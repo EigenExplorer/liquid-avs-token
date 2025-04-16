771bff6f8ce76a3886a4957d77c9f14be493aa46
Gowtham Sundaresan <131300352+gowthamsundaresan@users.noreply.github.com>
2025-04-16 16:48:17 +0200

    feat: deprecate off-chain price-updation (#115)

--- manager/src/index.ts
+++ manager/src/index.ts
@@ -3,18 +3,12 @@ import cron from "node-cron";
 
 import { stakeUnstakedAssets } from "./workflows/stakeUnstakedAssets";
 import { refreshDeployment } from "./workflows/refreshDeployment";
-import { updateAllTokenPrices } from "./workflows/updateAllTokenPrices";
 
 console.log("Initializing Restaking Manager ...");
 
 // Constants
 const MAX_RETRIES = 3;
 const RETRY_DELAY = 15 * 60;
-const PRICE_UPDATE_FREQUENCY = 720 * 60;
-
-function delay(seconds: number) {
-  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
-}
 
 /**
  * Daily responsibilities of restaking manager
@@ -54,48 +48,6 @@ async function dailyResponsibilities(retryCount = 0) {
     }
   }
 }
-
-/**
- * Manager plays the role of price-updater in v1
- *
- * @param retryCount
- */
-async function priceUpdater(retryCount = 0) {
-  while (true) {
-    try {
-      console.log(
-        `\n[Price Updater] Updating prices every ${PRICE_UPDATE_FREQUENCY} seconds:`
-      );
-
-      console.time("[Price Updater] Updated prices in");
-
-      await refreshDeployment();
-      await updateAllTokenPrices();
-
-      console.timeEnd("[Price Updater] Updated prices in");
-    } catch (error) {
-      console.log("[Price Updater] Failed to update prices at:", Date.now());
-      console.log(error);
-
-      if (retryCount < MAX_RETRIES) {
-        console.log(
-          `[Price Updater] Retrying in 15 minutes... (Attempt ${
-            retryCount + 1
-          } of ${MAX_RETRIES})`
-        );
-        setTimeout(() => priceUpdater(retryCount + 1), RETRY_DELAY * 1000);
-      } else {
-        console.log(
-          "[Price Updater] Max retries reached. Updating prices failed."
-        );
-      }
-    }
-    await delay(PRICE_UPDATE_FREQUENCY);
-  }
-}
-
-// Start price updation immediately
-priceUpdater();
 
 // 5 minutes past midnight every day
 cron.schedule("5 0 * * *", () => dailyResponsibilities());
