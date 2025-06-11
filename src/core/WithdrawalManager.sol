e0ffb87eb0d685adc7ba6e1650938e9ace7d7f89
Gowtham S <gowtham54321@yahoo.com>
2025-06-11 15:51:09 +0100

    feat: add withdrawal manager contract

---
+++ src/core/WithdrawalManager.sol
@@ -0,0 +1,300 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {Initializable} from "@openzeppelin-upgradeable/contracts/proxy/utils/Initializable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin-upgradeable/contracts/access/AccessControlUpgradeable.sol";
+import {ReentrancyGuardUpgradeable} from "@openzeppelin-upgradeable/contracts/security/ReentrancyGuardUpgradeable.sol";
+import {IStrategyManager} from "@eigenlayer/contracts/interfaces/IStrategyManager.sol";
+import {IDelegationManager} from "@eigenlayer/contracts/interfaces/IDelegationManager.sol";
+import {IDelegationManagerTypes} from "@eigenlayer/contracts/interfaces/IDelegationManager.sol";
+import {IStrategy} from "@eigenlayer/contracts/interfaces/IStrategy.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+
+import {IWithdrawalManager} from "../interfaces/IWithdrawalManager.sol";
+import {ILiquidToken} from "../interfaces/ILiquidToken.sol";
+import {ILiquidTokenManager} from "../interfaces/ILiquidTokenManager.sol";
+import {IStakerNode} from "../interfaces/IStakerNode.sol";
+import {IStakerNodeCoordinator} from "../interfaces/IStakerNodeCoordinator.sol";
+
+/// @title WithdrawalManager
+/// @notice Manages withdrawals between staker nodes and users
+/// @dev Implements IWithdrawalManager and uses OpenZeppelin's upgradeable contracts
+contract WithdrawalManager is IWithdrawalManager, Initializable, AccessControlUpgradeable, ReentrancyGuardUpgradeable {
+    using SafeERC20 for IERC20;
+    using Math for uint256;
+
+    // ------------------------------------------------------------------------------
+    // State
+    // ------------------------------------------------------------------------------
+
+    /// @notice EigenLayer contracts
+    IDelegationManager public delegationManager;
+
+    /// @notice LAT contracts
+    ILiquidToken public liquidToken;
+    ILiquidTokenManager public liquidTokenManager;
+    IStakerNodeCoordinator public stakerNodeCoordinator;
+
+    /// @notice User Withdrawals
+    mapping(bytes32 => WithdrawalRequest) public withdrawalRequests;
+    mapping(address => bytes32[]) public userWithdrawalRequests;
+
+    /// @notice Redemptions
+    mapping(bytes32 => ILiquidTokenManager.Redemption) public redemptions;
+
+    /// @notice The delay between user withdrawal request and ability to withdraw from this contract
+    uint256 public withdrawalDelay;
+
+    // ------------------------------------------------------------------------------
+    // Init functions
+    // ------------------------------------------------------------------------------
+
+    /// @dev Disables initializers for the implementation contract
+    constructor() {
+        _disableInitializers();
+    }
+
+    /// @inheritdoc IWithdrawalManager
+    function initialize(Init memory init) public initializer {
+        __AccessControl_init();
+        __ReentrancyGuard_init();
+
+        if (
+            address(init.initialOwner) == address(0) ||
+            address(init.liquidToken) == address(0) ||
+            address(init.delegationManager) == address(0) ||
+            address(init.liquidTokenManager) == address(0) ||
+            address(init.stakerNodeCoordinator) == address(0)
+        ) {
+            revert ZeroAddress();
+        }
+
+        _grantRole(DEFAULT_ADMIN_ROLE, init.initialOwner);
+
+        delegationManager = init.delegationManager;
+        liquidToken = init.liquidToken;
+        liquidTokenManager = init.liquidTokenManager;
+        stakerNodeCoordinator = init.stakerNodeCoordinator;
+
+        withdrawalDelay = 14 days;
+    }
+
+    // ------------------------------------------------------------------------------
+    // Core functions
+    // ------------------------------------------------------------------------------
+
+    /// @inheritdoc IWithdrawalManager
+    function createWithdrawalRequest(
+        IERC20[] memory assets,
+        uint256[] memory amounts,
+        address user,
+        bytes32 requestId
+    ) external override nonReentrant {
+        if (msg.sender != address(liquidToken)) revert NotLiquidToken(msg.sender);
+
+        WithdrawalRequest memory request = WithdrawalRequest({
+            user: user,
+            assets: assets,
+            requestedAmounts: amounts,
+            withdrawableAmounts: amounts,
+            requestTime: block.timestamp,
+            canFulfill: false
+        });
+
+        withdrawalRequests[requestId] = request;
+        userWithdrawalRequests[user].push(requestId);
+
+        emit WithdrawalInitiated(requestId, user, assets, amounts, block.timestamp);
+    }
+
+    /// @inheritdoc IWithdrawalManager
+    function fulfillWithdrawal(bytes32 requestId) external override nonReentrant {
+        WithdrawalRequest storage request = withdrawalRequests[requestId];
+
+        if (request.user == address(0)) revert InvalidWithdrawalRequest();
+        if (request.user != msg.sender) revert UnauthorizedAccess(msg.sender);
+        if (block.timestamp <= request.requestTime + withdrawalDelay) revert WithdrawalDelayNotMet();
+        if (request.canFulfill == false) revert WithdrawalNotReadyToFulfill();
+
+        for (uint256 i = 0; i < request.assets.length; i++) {
+            IERC20 asset = request.assets[i];
+            uint256 amount = request.withdrawableAmounts[i];
+
+            if (asset.balanceOf(address(this)) < amount) {
+                revert InsufficientBalance(asset, amount, asset.balanceOf(address(this)));
+            }
+        }
+
+        address user = request.user;
+        IERC20[] memory assets = request.assets;
+        uint256[] memory amounts = request.withdrawableAmounts;
+
+        delete withdrawalRequests[requestId];
+        bytes32[] storage userRequests = userWithdrawalRequests[user];
+        for (uint256 i = 0; i < userRequests.length; i++) {
+            if (userRequests[i] == requestId) {
+                userRequests[i] = userRequests[userRequests.length - 1];
+                userRequests.pop();
+                break;
+            }
+        }
+
+        // Fulfillment is complete
+        liquidToken.debitQueuedAssetBalances(assets, amounts);
+
+        for (uint256 i = 0; i < assets.length; i++) {
+            assets[i].safeTransfer(msg.sender, amounts[i]);
+        }
+
+        emit WithdrawalFulfilled(requestId, user, assets, amounts, block.timestamp);
+    }
+
+    /// @inheritdoc IWithdrawalManager
+    function recordRedemptionCreated(
+        bytes32 redemptionId,
+        ILiquidTokenManager.Redemption calldata redemption
+    ) external override {
+        if (msg.sender != address(liquidTokenManager)) revert NotLiquidTokenManager(msg.sender);
+
+        // Record the redemption
+        redemptions[redemptionId] = redemption;
+    }
+
+    /// @inheritdoc IWithdrawalManager
+    function recordRedemptionCompleted(
+        bytes32 redemptionId,
+        IERC20[] calldata receivedAssets,
+        uint256[] calldata receivedAmounts
+    ) external override returns (uint256[] memory) {
+        if (msg.sender != address(liquidTokenManager)) revert NotLiquidTokenManager(msg.sender);
+        if (receivedAmounts.length != receivedAssets.length) revert LengthMismatch();
+
+        ILiquidTokenManager.Redemption memory redemption = redemptions[redemptionId];
+
+        // We have already accounted for slashing up until the point of creating the withdrawal request on EL
+        // Now we compare the received amounts with the original withdrawable amounts (recorded during withdrawal request creation)
+        // to check for any slashing during the withdrawal queue period
+        uint256[] memory slashedFactors = new uint256[](receivedAssets.length);
+        uint256[] memory slashedAmounts = new uint256[](receivedAssets.length);
+
+        for (uint256 i = 0; i < receivedAssets.length; i++) {
+            uint256 originalWithdrawableAmount = 0;
+            bool assetFound = false;
+
+            for (uint256 j = 0; j < redemption.assets.length; j++) {
+                if (address(receivedAssets[i]) == address(redemption.assets[j])) {
+                    originalWithdrawableAmount = redemption.withdrawableAmounts[j];
+                    assetFound = true;
+                    break;
+                }
+            }
+
+            if (receivedAmounts[i] < originalWithdrawableAmount && originalWithdrawableAmount != 0) {
+                slashedFactors[i] = (receivedAmounts[i] * 1e18) / originalWithdrawableAmount;
+
+                slashedAmounts[i] = originalWithdrawableAmount - receivedAmounts[i];
+            } else {
+                slashedFactors[i] = 1e18;
+                slashedAmounts[i] = 0;
+            }
+        }
+
+        // Track the aggregated requested amounts per asset
+        uint256[] memory redemptionRequestedAmounts = new uint256[](receivedAssets.length);
+
+        // If the redemption is for user withdrawal, slash their withdrawable amounts by the slashing factor
+        if (
+            redemption.requestIds.length > 0 && withdrawalRequests[redemption.requestIds[0]].user != address(0) // If one user withdrawal is found, all requests are for user withdrawals
+        ) {
+            for (uint256 i = 0; i < redemption.requestIds.length; i++) {
+                bytes32 requestId = redemption.requestIds[i];
+                WithdrawalRequest storage request = withdrawalRequests[requestId];
+
+                for (uint256 j = 0; j < request.assets.length; j++) {
+                    for (uint256 k = 0; k < receivedAssets.length; k++) {
+                        if (address(request.assets[j]) == address(receivedAssets[k])) {
+                            uint256 originalAmount = request.requestedAmounts[j];
+                            redemptionRequestedAmounts[k] += originalAmount;
+
+                            // Slash the user's withdrawable amount by applying the slashed factor
+                            request.withdrawableAmounts[j] = Math.mulDiv(originalAmount, slashedFactors[k], 1e18);
+
+                            // Emit slashing event if amount was actually slashed
+                            if (slashedFactors[k] < 1e18) {
+                                emit UserSlashed(
+                                    requestId,
+                                    request.user,
+                                    request.assets[j],
+                                    originalAmount,
+                                    request.withdrawableAmounts[j]
+                                );
+                            }
+                            break;
+                        }
+                    }
+                }
+                // Mark withdrawal as ready to fulfill
+                request.canFulfill = true;
+            }
+        }
+
+        // Delete the redemption
+        delete redemptions[redemptionId];
+
+        // Account for withdrawal period slashing in queued withdrawal balances
+        // If the redemption is for rebalancing or undelegation, all internal accounting will now be complete after this
+        // If the redemption is for user withdrawals, the queued balances will still contain the withdrawable amounts
+        liquidToken.debitQueuedAssetBalances(receivedAssets, slashedAmounts);
+
+        return redemptionRequestedAmounts;
+    }
+
+    /// @inheritdoc IWithdrawalManager
+    function setWithdrawalDelay(uint256 newDelay) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (newDelay < 7 days || newDelay > 30 days) {
+            revert InvalidWithdrawalDelay(newDelay);
+        }
+
+        uint256 oldDelay = withdrawalDelay;
+        withdrawalDelay = newDelay;
+
+        emit WithdrawalDelayUpdated(oldDelay, newDelay);
+    }
+
+    // ------------------------------------------------------------------------------
+    // Getter functions
+    // ------------------------------------------------------------------------------
+
+    /// @inheritdoc IWithdrawalManager
+    function getUserWithdrawalRequests(address user) external view override returns (bytes32[] memory) {
+        return userWithdrawalRequests[user];
+    }
+
+    /// @inheritdoc IWithdrawalManager
+    function getWithdrawalRequests(
+        bytes32[] calldata requestIds
+    ) external view override returns (WithdrawalRequest[] memory) {
+        uint256 arrayLength = requestIds.length;
+        WithdrawalRequest[] memory requests = new WithdrawalRequest[](arrayLength);
+
+        for (uint256 i = 0; i < arrayLength; i++) {
+            WithdrawalRequest memory request = withdrawalRequests[requestIds[i]];
+            if (request.user == address(0)) revert WithdrawalRequestNotFound(requestIds[i]);
+            requests[i] = request;
+        }
+
+        return requests;
+    }
+
+    /// @inheritdoc IWithdrawalManager
+    function getRedemption(
+        bytes32 redemptionId
+    ) external view override returns (ILiquidTokenManager.Redemption memory) {
+        ILiquidTokenManager.Redemption memory redemption = redemptions[redemptionId];
+        if (redemption.requestIds.length == 0) revert RedemptionNotFound(redemptionId);
+
+        return redemption;
+    }
+}
---
+++ src/interfaces/IWithdrawalManager.sol
@@ -0,0 +1,207 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {IStrategyManager} from "@eigenlayer/contracts/interfaces/IStrategyManager.sol";
+import {IDelegationManager} from "@eigenlayer/contracts/interfaces/IDelegationManager.sol";
+import {IDelegationManagerTypes} from "@eigenlayer/contracts/interfaces/IDelegationManager.sol";
+import {IStrategy} from "@eigenlayer/contracts/interfaces/IStrategy.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {ILiquidToken} from "./ILiquidToken.sol";
+import {ILiquidTokenManager} from "./ILiquidTokenManager.sol";
+import {IStakerNodeCoordinator} from "./IStakerNodeCoordinator.sol";
+
+/// @title IWithdrawalManager Interface
+/// @notice Interface for managing withdrawals between staker nodes and users
+interface IWithdrawalManager {
+    // ============================================================================
+    // STRUCTS
+    // ============================================================================
+
+    /// @notice Initialization parameters for WithdrawalManager
+    /// @param initialOwner Address that will be granted DEFAULT_ADMIN_ROLE
+    /// @param delegationManager The EigenLayer DelegationManager contract
+    /// @param liquidToken The LiquidToken contract
+    /// @param liquidTokenManager The LiquidTokenManager contract
+    /// @param stakerNodeCoordinator The StakerNodeCoordinator contract
+    struct Init {
+        address initialOwner;
+        IDelegationManager delegationManager;
+        ILiquidToken liquidToken;
+        ILiquidTokenManager liquidTokenManager;
+        IStakerNodeCoordinator stakerNodeCoordinator;
+    }
+
+    /// @notice Represents a user's withdrawal request
+    /// @param user Address of the user requesting withdrawal
+    /// @param assets Array of token addresses being withdrawn
+    /// @param requestedAmounts Array of amounts being withdrawn per asset (in the unit of the asset)
+    /// @param withdrawableAmounts Array of amounts withdrawable per asset after any slashing (in the unit of the asset)
+    /// @param requestTime Timestamp when the withdrawal was requested
+    /// @param canFulfill Whether the withdrawal can be fulfilled by the user (set to true after redemption completion)
+    struct WithdrawalRequest {
+        address user;
+        IERC20[] assets;
+        uint256[] requestedAmounts;
+        uint256[] withdrawableAmounts;
+        uint256 requestTime;
+        bool canFulfill;
+    }
+
+    // ============================================================================
+    // EVENTS
+    // ============================================================================
+
+    /// @notice Emitted when a user initiates a withdrawal request
+    /// @param requestId Unique identifier for the withdrawal request
+    /// @param user Address of the user requesting withdrawal
+    /// @param assets Array of token addresses being withdrawn
+    /// @param amounts Array of amounts being withdrawn per asset
+    /// @param timestamp Block timestamp when the request was made
+    event WithdrawalInitiated(
+        bytes32 indexed requestId,
+        address indexed user,
+        IERC20[] assets,
+        uint256[] amounts,
+        uint256 timestamp
+    );
+
+    /// @notice Emitted when a user's withdrawal request is fulfilled
+    /// @param requestId Unique identifier for the withdrawal request
+    /// @param user Address of the user whose withdrawal was fulfilled
+    /// @param assets Array of token addresses that were withdrawn
+    /// @param amounts Array of token amounts that were withdrawn
+    /// @param timestamp Block timestamp when the fulfillment occurred
+    event WithdrawalFulfilled(
+        bytes32 indexed requestId,
+        address indexed user,
+        IERC20[] assets,
+        uint256[] amounts,
+        uint256 timestamp
+    );
+
+    /// @notice Emitted when a user is slashed and the slashing is applied to the corresponding withdrawable amount on their withdrawal request
+    /// @param requestId Unique identifier for the withdrawal request
+    /// @param user Address of the user whose withdrawal was fulfilled
+    /// @param asset The ERC20 token that was slashed
+    /// @param originalAmount The original withdrawal amount requested by the user on creating the withdrawal request
+    /// @param withdrawableAmount The final withdrawable amount after slashing
+    event UserSlashed(
+        bytes32 indexed requestId,
+        address indexed user,
+        IERC20 indexed asset,
+        uint256 originalAmount,
+        uint256 withdrawableAmount
+    );
+
+    /// @notice Emitted when the withdrawal delay is updated
+    /// @param oldDelay Previous withdrawal delay value
+    /// @param newDelay Newly updated withdrawal delay value
+    event WithdrawalDelayUpdated(uint256 oldDelay, uint256 newDelay);
+
+    // ============================================================================
+    // CUSTOM ERRORS
+    // ============================================================================
+
+    /// @notice Error thrown when a zero address is provided where a non-zero address is required
+    error ZeroAddress();
+
+    /// @notice Error thrown when a function restricted to LiquidToken is called by another address
+    /// @param sender Address that attempted the call
+    error NotLiquidToken(address sender);
+
+    /// @notice Error thrown when a function restricted to LiquidTokenManager is called by another address
+    /// @param sender Address that attempted the call
+    error NotLiquidTokenManager(address sender);
+
+    /// @notice Error thrown when array lengths don't match in function parameters
+    error LengthMismatch();
+
+    /// @notice Error thrown when a withdrawal request is invalid
+    error InvalidWithdrawalRequest();
+
+    /// @notice Error for unauthorized access
+    error UnauthorizedAccess(address sender);
+
+    /// @notice Error thrown when a redemption is invalid
+    error InvalidRedemption();
+
+    /// @notice Error thrown when attempting to fulfill a withdrawal before the delay period
+    error WithdrawalDelayNotMet();
+
+    /// @notice Error thrown when withdrawal cannot be fulfilled yet (redemption not completed)
+    error WithdrawalNotReadyToFulfill();
+
+    /// @notice Error when withdrawal delay value was attempted with an invalid delay value
+    error InvalidWithdrawalDelay(uint256 delay);
+
+    /// @notice Error thrown when a withdrawal request ID doesn't exist
+    /// @param requestId The withdrawal request ID that wasn't found
+    error WithdrawalRequestNotFound(bytes32 requestId);
+
+    /// @notice Error thrown when a redemption ID doesn't exist
+    /// @param redemptionId The redemption ID that wasn't found
+    error RedemptionNotFound(bytes32 redemptionId);
+
+    /// @notice Error thrown when contract has insufficient balance for an operation
+    /// @param asset The token address
+    /// @param required The amount required
+    /// @param available The amount available
+    error InsufficientBalance(IERC20 asset, uint256 required, uint256 available);
+
+    // ============================================================================
+    // FUNCTIONS
+    // ============================================================================
+
+    /// @notice Initializes the contract
+    /// @param init Initialization parameters
+    function initialize(Init memory init) external;
+
+    /// @notice Creates a withdrawal request for a user when they initate one via `LiquidToken`
+    /// @param assets The final assets the the user wants to end up with
+    /// @param amounts The withdrawal amounts per asset
+    /// @param user The requesting user's address
+    /// @param requestId The unique identifier of the withdrawal request
+    function createWithdrawalRequest(
+        IERC20[] memory assets,
+        uint256[] memory amounts,
+        address user,
+        bytes32 requestId
+    ) external;
+
+    /// @notice Allows users to fulfill a withdrawal request after the delay period and receive all corresponding funds
+    /// @param requestId The unique identifier of the withdrawal request
+    function fulfillWithdrawal(bytes32 requestId) external;
+
+    /// @notice Called by `LiquidTokenManger` when a new redemption is created
+    /// @param redemptionId The unique identifier of the redemption
+    /// @param redemption The details of the redemption
+    function recordRedemptionCreated(bytes32 redemptionId, ILiquidTokenManager.Redemption calldata redemption) external;
+
+    /// @notice Called by `LiquidTokenManger` when a redemption is completed
+    /// @dev If there was any slashing during the withdrawal queue period, its accounting is handled here
+    /// @param redemptionId The ID of the redemption
+    /// @param receivedAssets The set of assets that received from all EL withdrawals
+    /// @param receivedAmounts Total amounts per for `receivedAssets` (after any slashing)
+    function recordRedemptionCompleted(
+        bytes32 redemptionId,
+        IERC20[] calldata assets,
+        uint256[] calldata receivedAmounts
+    ) external returns (uint256[] memory);
+
+    /// @notice Updates the withdrawal delay period
+    /// @param newDelay The new withdrawal delay in seconds
+    function setWithdrawalDelay(uint256 newDelay) external;
+
+    /// @notice Returns all withdrawal request IDs for a given user
+    /// @param user The address of the user
+    function getUserWithdrawalRequests(address user) external view returns (bytes32[] memory);
+
+    /// @notice Returns all withdrawal request details for a set of request IDs
+    /// @param requestIds The IDs of the withdrawal requests
+    function getWithdrawalRequests(bytes32[] calldata requestIds) external view returns (WithdrawalRequest[] memory);
+
+    /// @notice Returns all redemption details for a given redemption ID
+    /// @param redemptionId The ID of the redemption
+    function getRedemption(bytes32 redemptionId) external view returns (ILiquidTokenManager.Redemption memory);
+}
